47e01eba357161037763d8e09e5b3ac80895acf5
EldarMuhamethanov <eldarmy@mail.ru>
2020-04-13 16:16:24 +0300

    delete test controller

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class RegistrationController extends AbstractController
 {
     /**
-     * @Route("/registration", name="registration")
+     * @Route("/registration")
      */
     public function index()
     {
